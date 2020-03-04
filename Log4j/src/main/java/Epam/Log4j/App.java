ed63b1b476be583e2ae56526679449a84acc6414
lenovo <lenovo@DESKTOP-NN19MM0>
2020-03-04 23:49:55 +0530

    Log4j

---
+++ Log4j/src/main/java/Epam/Log4j/App.java
@@ -0,0 +1,13 @@
+package Epam.Log4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
